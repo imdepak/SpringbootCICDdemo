e1de82ba86a534e9c4ffb47e051dfa863e34d56b
imdepak <deepak.v@outlook.com>
2024-06-03 18:02:26 +0530

    myfirstcommit

---
+++ src/main/java/com/example/spring/docker/project/SpringbootDockerProjectApplication.java
@@ -0,0 +1,23 @@
+package com.example.spring.docker.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class SpringbootDockerProjectApplication {
+	
+	@GetMapping("/message")
+	public String getMessage()
+	{
+		return "Hello..I am From Docker";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring/docker/project/SpringbootDockerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.docker.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
