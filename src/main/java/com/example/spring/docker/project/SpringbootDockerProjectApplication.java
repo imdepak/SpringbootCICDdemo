778e1a111914902a96ebe83e7f72a560c3f05b3f
imdepak <deepak.v@outlook.com>
2024-06-11 16:32:42 +0530

    demo2

--- src/main/java/com/example/spring/docker/project/SpringbootDockerProjectApplication.java
+++ src/main/java/com/example/spring/docker/project/SpringbootDockerProjectApplication.java
@@ -13,7 +13,7 @@ public class SpringbootDockerProjectApplication {
 	@GetMapping("/message")
 	public String getMessage()
 	{
-		return "Hello..I am From AWS CI/CD Pipeline";
+		return "Hello..I am From AWS CI/CD Pipeline Release 2";
 	}
 
 	public static void main(String[] args) {
